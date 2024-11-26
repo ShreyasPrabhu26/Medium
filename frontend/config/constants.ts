8aa2bcce784379fae40320654a6b35a35c01d08b
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2024-11-26 20:37:38 +0530

    [IMPLEMENT] Sign Up

---
+++ frontend/config/constants.ts
@@ -0,0 +1,2 @@
+// export const BACKEND_BASE_URL = "https://medium.shreyasprabhu26.workers.dev";
+export const BACKEND_BASE_URL = "http://localhost:8787";
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,23 +1,41 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import axios from "axios";
 
 import AuthHeader from "../components/AuthHeader";
-
 import { SignupInput } from "@shreyasprabhu26/medium-common";
 
+import { BACKEND_BASE_URL } from "../../config/constants";
+
 const Signup = () => {
-  const signUpPayLoad: SignupInput = useState({
+  const navigate = useNavigate();
+  const [signUpPayLoad, setSignUpPayLoad] = useState<SignupInput>({
     username: "",
     email: "",
     password: "",
   });
+
+  const handleUserSignUp = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        `${BACKEND_BASE_URL}/api/v1/user/signup`,
+        signUpPayLoad
+      );
+      localStorage.setItem("token", response?.data?.token);
+      navigate("/blogs");
+    } catch (error) {
+      console.error("Error signing up:", error);
+    }
+  };
 
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col">
         <AuthHeader />
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body">
+          <form className="card-body" onSubmit={handleUserSignUp}>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Username</span>
@@ -28,6 +46,13 @@ const Signup = () => {
                 className="input input-bordered"
                 autoFocus
                 required
+                value={signUpPayLoad.username}
+                onChange={(e) =>
+                  setSignUpPayLoad({
+                    ...signUpPayLoad,
+                    username: e.target.value,
+                  })
+                }
               />
             </div>
             <div className="form-control">
@@ -39,6 +64,10 @@ const Signup = () => {
                 placeholder="Email"
                 className="input input-bordered"
                 required
+                value={signUpPayLoad.email}
+                onChange={(e) =>
+                  setSignUpPayLoad({ ...signUpPayLoad, email: e.target.value })
+                }
               />
             </div>
             <div className="form-control">
@@ -50,10 +79,19 @@ const Signup = () => {
                 placeholder="Password"
                 className="input input-bordered"
                 required
+                value={signUpPayLoad.password}
+                onChange={(e) =>
+                  setSignUpPayLoad({
+                    ...signUpPayLoad,
+                    password: e.target.value,
+                  })
+                }
               />
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Sign Up</button>
+              <button className="btn btn-primary" type="submit">
+                Sign Up
+              </button>
             </div>
           </form>
           <footer className="text-center mb-5 space-x-5">
